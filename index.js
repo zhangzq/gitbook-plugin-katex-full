00e39836a79eb114d4d460b55ce22982f212c306
zhiqiang <zhang@zhiqiang.org>
2016-09-13 19:24:44 +0800

    small

--- index.js
+++ index.js
@@ -1,49 +1,56 @@
 var katex = require("katex");
 
 module.exports = {
-    book: {
-        assets: "./static",
-        js: [],
-        css: [
-            "katex.min.css"
-        ]
-    },
-    ebook: {
-        assets: "./static",
-        css: [
-            "katex.min.css"
-        ]
-    },
-    blocks: {
-        display_math: {
-            shortcuts: {
-                parsers: ["markdown", "asciidoc", "restructuredtext"],
-                start: "$$",
-                end: "$$"
-            },
-            process: function(blk) {
-                var tex = blk.body;
-                var output = katex.renderToString(tex, {
-                    displayMode: true
-                });
+    // book: {
+        // assets: "./static",
+        // js: [],
+        // css: [
+            // "katex.min.css"
+        // ]
+    // },
+    // ebook: {
+        // assets: "./static",
+        // css: [
+            // "katex.min.css"
+        // ]
+    // },
+    // blocks: {
+        // display_math: {
+            // shortcuts: {
+                // parsers: ["markdown"],
+                // start: "$$",
+                // end: "$$"
+            // },
+            // process: function(blk) {
+                // var tex = blk.body;
+                // var output = katex.renderToString(tex, {
+                    // displayMode: true
+                // });
 
-                return output;
-            }
-        },
-        inline_math: {
-            shortcuts: {
-                parsers: ["markdown", "asciidoc", "restructuredtext"],
-                start: "$",
-                end: "$"
-            },
-            process: function(blk) {
-                var tex = blk.body;
-                var output = katex.renderToString(tex, {
-                    displayMode: false
-                });
+                // return output;
+            // }
+        // },
+        // inline_math: {
+            // shortcuts: {
+                // parsers: ["markdown"],
+                // start: "\\(",
+                // end: "\\)"
+            // },
+            // process: function(blk) {
+                // var tex = blk.body;
+                // var output = katex.renderToString(tex, {
+                    // displayMode: false
+                // });
 
-                return output;
-            }
-        },
+                // return output;
+            // }
+        // },
+    // }
+    hooks: {
+        "page": function(section) {
+            section.content += '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css"> <script src="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js"></script>script>';
+
+            return section;
+        }
     }
 };
